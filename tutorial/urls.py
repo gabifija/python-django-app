d2b547df1e4c7113a6f6960ffa8ff4187e1b8cf5
Gabi <gabrielafijalkowska@gmail.com>
2018-02-04 18:16:31 +0100

    Preparing URLS for the registration

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
-
+from tutorial import views
 urlpatterns = [
+    url(r'^$', views.login_redirect, name='login_redirect'),
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('accounts.urls')),
 ]
