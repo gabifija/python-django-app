a024d4a5df370b820c8516252fa364bc0ff6c297
Gabi <gabrielafijalkowska@gmail.com>
2018-02-04 18:30:52 +0100

    Added user registration form

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^$', views.home),
     url(r'^login/$', login, {'template_name': 'accounts/login.html'}),
     url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'}),
+    url(r'^register/$', views.register, name='register')
 ]
