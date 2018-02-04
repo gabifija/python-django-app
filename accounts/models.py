c2f08538ae604036f8906ee225c40118475f313a
Gabi <gabrielafijalkowska@gmail.com>
2018-02-04 17:31:56 +0100

    Added django signals to trigger code

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
 class UserProfile(models.Model):
@@ -8,3 +9,9 @@ class UserProfile(models.Model):
     city = models.CharField(max_length=100, default='')
     webstie = models.URLField(default='')
     phone = models.IntegerField(default=0)
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
